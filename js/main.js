13156eb628056538adba09b47fcfe1b896c93519
LesPaul997 <mariogiordano941@gmail.com>
2024-05-05 17:42:12 +0200

    add code js whit password

--- js/main.js
+++ js/main.js
@@ -21,7 +21,46 @@ console.log(variableNew); */
 document.getElementById("InputPassword") = password; */
 
 
+/* const firstName = prompt ('Inserisci il tuo nome');
+const lastName = prompt ('Inserisci il tuo sognome');
+const age = prompt ('Inserisci la età');
+const color = prompt ('Inserisci il tuo colore preferito');
+
+const output = `
+
+nome: $(firstName)
+cognome: $(lastName)
+età: $(age)
+colore: $(color)
+`;
 
 
 
+document.getElementById('nome-utente').innerHTML = firstName;
+document.getElementById('cognome-utente').innerHTML = lastName;
+document.getElementById('eta').innerHTML = age;
+document.getElementById('colore').innerHTML = color;
 
+
+console.log(output); */
+
+/*------------------------------------------------------------------------------------------------*/
+       /*    ^^^SOPRA SONO TUTTI ESEMPI PER INSERIRE ALL'INTERNO DI UN LAYOUT^^^       */
+
+
+
+const firstName = prompt('Inserisci il tuo nome');
+const lastName = prompt('Inserisci il tuo cognome');
+const color = prompt('Inserisci il tuo colore preferito');
+/* const age = prompt('Inserisci la tua età'); */
+
+
+
+console.log(firstName + lastName + color + "21");
+
+document.getElementById('nome-utente').innerHTML = firstName;
+document.getElementById('cognome-utente').innerHTML = lastName;
+/* document.getElementById('colore-preferito').innerHTML = color;
+document.getElementById('eta-utente').innerHTML = age; */
+
+/* document.getElementById('password-segreta').innerHTML = PasswordSegreta; */
